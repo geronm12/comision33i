9764a4eb97b8bf26b1809afc00008775a9bd2752
Gerónimo <gerolpz01@gmail.com>
2023-02-14 20:03:20 -0300

    clase  14/07/2023 - switch - for

---
+++ 03_ciclos_switch.js
@@ -0,0 +1,44 @@
+// const h1 = "<h1>Hola Mundo </h1>";
+// const style = "h1 { color: red; }";
+
+// let numero_cancha = 5;
+// let precio_cancha = 120;
+// let esta_alquilada = true;
+// let es_techada = true;
+// let clima = "lluvioso";
+
+// // La cancha si o si debe estár libre, es decir esta_alquilada debe ser false
+// // Si el clima es lluvioso la cancha debe ser techada si o si
+// // Si el clima es soleado la cancha puede ser techada o no
+
+// if (esta_alquilada === false) {
+//   if (clima === "lluvioso" && es_techada === true) {
+//     //vendo la cancha
+//   } else {
+//     //no vendo la cancha
+//   }
+// }
+
+// //!==
+// if (esta_alquilada !== true) {
+//   if (clima === "lluvioso") {
+//     if (es_techada === false) {
+//     }
+//   }
+// }
+
+// if (!esta_alquilada) {
+//   if (clima === "lluvioso") {
+//     if (!es_techada) {
+//       //no alquilo
+//     } else {
+//       //si alquilo
+//     }
+//   }
+// }
+
+// //DOM
+
+const h1 = document.getElementById("h");
+
+console.log(h1);
